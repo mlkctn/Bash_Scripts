486d3d4e1456e4ea9d71a5b8508a003ec15588f5
mlkctn <melik_cetin@hotmail.com>
2025-05-28 12:18:36 +0200

    Remove Log files with.gz extention

---
+++ Bash-Scripts/Basic-Scripts/removel-log-files.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Find all .gz files under /var/log, excluding anything with "private" in the path
+files=$(find /var/log -type f  -name "*.gz"  ! -path "*private*")
+
+# If no files found, exit
+if [ -z "$files" ]; then
+        echo "No .gz files found."
+        exit 0
+
+fi
+
+# Show files
+echo "The following .gz files will be deleted."
+echo "$files"
+echo
+
+# Ask for confirmation
+read -p "Do you want to delete all of these files? (y/n): " answer
+
+if [[ "$answer" == "y" ]]; then
+        echo "$files" | xargs rm -v
+        echo "Files deleted."
+else
+        echo "Aborted. No files deleted."
+fi
+
